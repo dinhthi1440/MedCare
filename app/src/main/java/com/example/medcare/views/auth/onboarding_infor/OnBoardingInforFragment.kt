259e41eef6068a7c9bfe41b2152cbd29b29db43c
Dinh Thi <dinhthi1440@gmail.com>
2025-05-15 18:32:02 +0700

    add onboarding infor

---
+++ app/src/main/java/com/example/medcare/views/auth/onboarding_infor/OnBoardingInforFragment.kt
@@ -0,0 +1,26 @@
+package com.example.medcare.views.auth.onboarding_infor
+
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import com.example.medcare.base.BaseFragment
+import com.example.medcare.databinding.FragmentOnBoardingInforBinding
+import com.example.medcare.views.auth.AuthViewModel
+
+class OnBoardingInforFragment : BaseFragment<FragmentOnBoardingInforBinding>(FragmentOnBoardingInforBinding::inflate) {
+    override val viewModel by viewModel<AuthViewModel>()
+
+    override fun initData() {
+
+    }
+
+    override fun handleEvent() {
+
+    }
+
+    override fun bindData() {
+
+    }
+
+    override fun destroy() {
+
+    }
+}
